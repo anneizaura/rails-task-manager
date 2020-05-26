1ed46c77f6345c62f919e09cf393434663f0c583
Anne Costa <anneizaura@gmail.com>
2020-05-26 16:02:23 -0300

    tasks 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
+ # CRUD
+ # READ index varias tarefas e o show 1 tarefa
+   # verb path, to: 'control#action'
+  # READ
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/:id', to: 'tasks#show'
+
+  # # CREATE
+  # # 1. rota pra levar o usuario ao formulário
+  get '/tasks/new', to: 'tasks#new'
+
+  # # 2. rota pra levar as infos que o usuário inputou p/ o controller
+  post '/tasks', to: 'tasks#create'
+
+  # # UPDATE
+  # # 1. rota pra levar o usuario ao formulário
+  get '/tasks/:id/edit', to: 'tasks#edit'
+
+  # # 2. rota pra levar as infos que o usuário inputou p/ o controller
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # # DELETE
+  delete '/tasks/:id', to: 'tasks#delete'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
